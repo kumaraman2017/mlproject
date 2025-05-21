a2d3c4eb17ad485b2d0d1caa22959bba38d24aa5
Aman Kumar <kumar.amann126@gmail.com>
2025-05-21 11:37:06 +0530

    Setting up Pipeline

--- src/utilis.py
+++ src/utilis.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -20,15 +21,20 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,params):
     try:
         report={}
         
         for name,model in models.items():
-          
+            para=params[name]
             
+            
+            
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
-            
+
             y_train_pred=model.predict(X_train)
             y_test_pred=model.predict(X_test)
             train_model_score=r2_score(y_train,y_train_pred)
@@ -36,5 +42,13 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
             report[name]=test_model_score
             
         return report
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+        
     except Exception as e:
         raise CustomException(e,sys)
